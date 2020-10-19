86527578ab66e5d393b35c586b094857ba9f641c
Nathan <nathanmersha@gmail.com>
2020-10-19 19:51:35 +0300

    Added nunito font and initialized repo.

---
+++ android/app/src/main/java/com/awramarket/captain_order_management/MainActivity.java
@@ -0,0 +1,13 @@
+package com.awramarket.captain_order_management;
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+    GeneratedPluginRegistrant.registerWith(flutterEngine);
+  }
+}
