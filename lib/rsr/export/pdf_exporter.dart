0632315df0f1426d2ca226bdfefa27c8dfdc28e8
Nathan <nathanmersha@gmail.com>
2021-02-21 13:08:14 +0300

    Added style to pdf exporter functions

--- lib/rsr/export/pdf_exporter.dart
+++ lib/rsr/export/pdf_exporter.dart
@@ -1,4 +1,3 @@
-import 'dart:convert';
 import 'dart:typed_data';
 import 'dart:ui';
 import 'package:captain/db/model/personnel.dart';
@@ -16,31 +15,32 @@ class Exporter {
 
   Document pdf = Document();
 
-  TextStyle companyValuesStyle() {
-    return TextStyle(color: PdfColors.black, fontSize: 12, );
+  TextStyle companyValuesStyle(ttf) {
+    return TextStyle(color: PdfColors.black, fontSize: 12, font: ttf);
   }
 
-  TextStyle getValueStyle() {
-    return TextStyle(color: PdfColor.fromInt(0xff404040), fontSize: 16, );
+  TextStyle getValueStyle(ttf) {
+    return TextStyle(color: PdfColor.fromInt(0xff404040), fontSize: 16, font: ttf);
   }
 
-  TextStyle getTableTitleStyle() {
-    return TextStyle(fontSize: 20, fontWeight: FontWeight.bold, );
+  TextStyle getTableTitleStyle(ttf) {
+    return TextStyle(fontSize: 20, fontWeight: FontWeight.bold, font: ttf);
   }
 
-  TextStyle getTitleStyle() {
-    return TextStyle(fontSize: 13, lineSpacing: 10, fontWeight: FontWeight.bold, color: PdfColor.fromInt(0xff404040));
+  TextStyle getTitleStyle(ttf) {
+    return TextStyle(fontSize: 13, lineSpacing: 10, fontWeight: FontWeight.bold, font: ttf);
   }
 
   Future<bool> toPdf(
       {Personnel customer,
-      List<Product> products,
-      num totalAmount,
-      num advanceAmount,
-      num remainingAmount,
-      DateTime lastModified,
-      mat.BuildContext context}) async {
-
+        List<Product> products,
+        num totalAmount,
+        num advanceAmount,
+        num remainingAmount,
+        DateTime lastModified,
+        mat.BuildContext context}) async {
+    ByteData data = await rootBundle.load('assets/fonts/OpenSans-Regular.ttf');
+    var ttf = Font.ttf(data);
 
     pdf.addPage(Page(
         pageFormat: PdfPageFormat.a4,
@@ -61,11 +61,12 @@ class Exporter {
                           Text("Kapci Coatings",
                               style: TextStyle(
                                 fontSize: 19,
+                                font: ttf,
                               )),
-                          Text("Address : Gofa , Addis Ababa", style: companyValuesStyle(), textAlign: TextAlign.right),
-                          Text("Tel : +251911780428", style: companyValuesStyle()),
-                          Text("www.kemsadhub.com", style: companyValuesStyle()),
-                          Text(DateFormat.yMMMd().format(lastModified), style: companyValuesStyle()),
+                          Text("Address : Gofa , Addis Ababa", style: companyValuesStyle(ttf), textAlign: TextAlign.right),
+                          Text("Tel : +251911780428", style: companyValuesStyle(ttf)),
+                          Text("www.kemsadhub.com", style: companyValuesStyle(ttf)),
+                          Text(DateFormat.yMMMd().format(lastModified), style: companyValuesStyle(ttf)),
                         ],
                       )
                     ],
@@ -80,11 +81,11 @@ class Exporter {
                 SizedBox(height: 8),
 
                 Table(children: [
-                  TableRow(children: [Text("Name"), Text(customer.name)]),
-                  TableRow(children: [Text("Phone number"), Text(customer.phoneNumber)]),
-                  TableRow(children: [Text("Address"), Text(customer.address ?? "-")]),
-                  TableRow(children: [Text("Address Detail"), Text(customer.addressDetail ?? "-")]),
-                  TableRow(children: [Text("Email"), Text(customer.email ?? "-")])
+                  TableRow(children: [Text("Name"), Text(customer.name, style: getTitleStyle(ttf))]),
+                  TableRow(children: [Text("Phone number"), Text(customer.phoneNumber, style: getTitleStyle(ttf))]),
+                  TableRow(children: [Text("Address"), Text(customer.address ?? "-", style: getTitleStyle(ttf))]),
+                  TableRow(children: [Text("Address Detail"), Text(customer.addressDetail ?? "-", style: getTitleStyle(ttf))]),
+                  TableRow(children: [Text("Email"), Text(customer.email ?? "-", style: getTitleStyle(ttf))])
                 ]),
 
                 SizedBox(height: 28),
@@ -93,7 +94,7 @@ class Exporter {
                     alignment: Alignment.centerLeft),
                 SizedBox(height: 8),
 
-                Table(border: TableBorder(color: PdfColor.fromInt(0xffbfbfbf)), children: createProductRow(products,)),
+                Table(border: TableBorder(color: PdfColor.fromInt(0xffbfbfbf)), children: createProductRow(products, ttf)),
                 SizedBox(height: 28),
                 Align(
                     child: Text("Payment", textAlign: TextAlign.right, style: TextStyle(fontWeight: FontWeight.bold)),
@@ -129,15 +130,47 @@ class Exporter {
 
 
   Future<bool> toPdfProductSold({
-        List<ProductSoldStat> productSoldStat,
-        DateTime from,
-        DateTime to,
-        mat.BuildContext context}) async {
+    List<ProductSoldStat> productSoldStat,
+    DateTime from,
+    DateTime to,
+    mat.BuildContext context}) async {
+    ByteData data = await rootBundle.load('assets/fonts/OpenSans-Regular.ttf');
+    // final Uint8List fontDataBold = File('open-sans-bold.ttf').readAsBytesSync();
+    // final Font ttfBold = Font.ttf(fontData.buffer.asByteData());
 
+    var ttf = Font.ttf(data);
     DateFormat dateFormat = DateFormat("dd-MMM-yyyy");
 
-    print("to pdf product sold : $productSoldStat");
 
+
+    int maxElement = 25;
+    if(productSoldStat.length <= maxElement){
+      print("Here it's less than");
+      createPageSoldProduct(ttf, dateFormat, from, to, productSoldStat, 0, productSoldStat.length);
+    }else{
+      int firstIndex = 0;
+      int lastIndex = 0;
+      bool done = false;
+      print("product length : ${productSoldStat.length}-------------");
+      while(!done){
+        print("Here looping");
+        firstIndex = lastIndex;
+        lastIndex = lastIndex + maxElement;
+        if(lastIndex > productSoldStat.length){
+          lastIndex = productSoldStat.length;
+          done = true;
+        }
+        createPageSoldProduct(ttf, dateFormat, from, to, productSoldStat, firstIndex, lastIndex);
+      }
+
+    }
+   
+
+    await Printing.layoutPdf(onLayout: (PdfPageFormat format) async => pdf.save());
+    return true;
+  }
+
+  void createPageSoldProduct(Font ttf, DateFormat dateFormat, DateTime from, DateTime to, List<ProductSoldStat> productSoldStat, int start, int end) {
     pdf.addPage(Page(
         pageFormat: PdfPageFormat.a4,
         margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
@@ -157,59 +190,54 @@ class Exporter {
                           Text("Kapci Coatings",
                               style: TextStyle(
                                 fontSize: 19,
-
+                                font: ttf,
                               )),
-                          Text("Address : Gofa , Addis Ababa", style: companyValuesStyle(), textAlign: TextAlign.right),
-                          Text("Tel : +251911780428", style: companyValuesStyle()),
-                          Text("www.kemsadhub.com", style: companyValuesStyle()),
-                          Text(DateFormat.yMMMd().format(DateTime.now()), style: companyValuesStyle()),
+                          Text("Address : Gofa , Addis Ababa", style: companyValuesStyle(ttf), textAlign: TextAlign.right),
+                          Text("Tel : +251911780428", style: companyValuesStyle(ttf)),
+                          Text("www.kemsadhub.com", style: companyValuesStyle(ttf)),
+                          Text(DateFormat.yMMMd().format(DateTime.now()), style: companyValuesStyle(ttf)),
                         ],
                       )
                     ],
                   ),
                 ),
-
+    
                 Divider(),
                 SizedBox(height: 20),
                 Align(
                     child: Text("Time range", textAlign: TextAlign.right, style: TextStyle(fontWeight: FontWeight.bold)),
                     alignment: Alignment.centerLeft),
                 SizedBox(height: 8),
-
+    
                 Table(children: [
                   TableRow(children: [Text("From"), Text(dateFormat.format(from))]),
                   TableRow(children: [Text("To"), Text(dateFormat.format(to))]),
-
+    
                 ]),
-
-                SizedBox(height: 8),
-                Align(
-                    child: Text("Normal Orders", textAlign: TextAlign.right, style: TextStyle(fontWeight: FontWeight.bold)),
-                    alignment: Alignment.centerLeft),
-                SizedBox(height: 8),
-
-                Table(border: TableBorder(color: PdfColor.fromInt(0xffbfbfbf)), children: createProductSoldStat(productSoldStat)),
+    
                 SizedBox(height: 28),
                 Align(
-                    child: Text("Payment", textAlign: TextAlign.right, style: TextStyle(fontWeight: FontWeight.bold)),
+                    child: Text("Orders", textAlign: TextAlign.right, style: TextStyle(fontWeight: FontWeight.bold)),
                     alignment: Alignment.centerLeft),
                 SizedBox(height: 8),
+    
+                Table(border: TableBorder(color: PdfColor.fromInt(0xffbfbfbf)), children: createProductSoldStat(productSoldStat.sublist(start,end), ttf)),
+                SizedBox(height: 28),
 
-                Table(children: [
+                SizedBox(height: 8),
+    
+                end == productSoldStat.length ? Table(children: [
                   TableRow(children: [
                     Text("Total"),
                     Text("${getSum(productSoldStat)} br", style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold))
                   ]),
-                ]),
-
+                ]) : Container(),
+    
                 // Content
               ],
             ),
           );
         }));
-
-    await Printing.layoutPdf(onLayout: (PdfPageFormat format) async => pdf.save());
-    return true;
   }
 
   String getSum(List<ProductSoldStat> productSoldStats){
@@ -220,56 +248,64 @@ class Exporter {
     return sum.toStringAsFixed(2);
   }
 
-  List<TableRow> createProductRow(List<Product> products) {
+  List<TableRow> createProductRow(List<Product> products, var ttf) {
     List<TableRow> tableRow = [];
 
     tableRow.add(TableRow(children: [
-      Text("Name", style: getTitleStyle()),
-      Text("Type", style: getTitleStyle()),
-      Text("Paint type", style: getTitleStyle()),
-      Text("Quantity", style: getTitleStyle()),
-      Text("Unit", style: getTitleStyle()),
-      Text("SubTotal", style: getTitleStyle()),
-      Text("Status", style: getTitleStyle()),
+      Text("Name", style: getTitleStyle(ttf)),
+      Text("Type", style: getTitleStyle(ttf)),
+      Text("Paint type", style: getTitleStyle(ttf)),
+      Text("Quantity", style: getTitleStyle(ttf)),
+      Text("Unit", style: getTitleStyle(ttf)),
+      Text("SubTotal", style: getTitleStyle(ttf)),
+      Text("Status", style: getTitleStyle(ttf)),
     ]));
 
     tableRow.addAll(products.map((Product p) {
       return TableRow(children: [
-        Text(p.name),
-        Text(p.type ?? "-"),
-        Text(p.paintType ?? "-"),
-        Text("${p.quantityInCart.toString()}${p.unitOfMeasurement}"),
-        Text("${oCCy.format(p.unitPrice)}br"),
-        Text("${oCCy.format(p.subTotal)}br"),
-        Text(p.status ?? "-"),
+        Text(p.name, style: getTitleStyle(ttf)),
+        Text(p.type ?? "-", style: getTitleStyle(ttf)),
+        Text(p.paintType ?? "-", style: getTitleStyle(ttf)),
+        Text("${p.quantityInCart.toString()}${p.unitOfMeasurement}", style: getTitleStyle(ttf)),
+        Text("${oCCy.format(p.unitPrice)} br", style: getTitleStyle(ttf)),
+        Text("${oCCy.format(p.subTotal)} br", style: getTitleStyle(ttf)),
+        Text(mapStatus(p.status) ?? "-", style: getTitleStyle(ttf)),
       ]);
     }).toList());
 
     return tableRow;
   }
+  String mapStatus(String old){
+    if(old == "c_Delivered"){
+      return "Delivered";
+    }else if(old == "b_Completed"){
+      return "Completed";
+    }else{
+      return "Pending";
+    }
+  }
 
-  List<TableRow> createProductSoldStat(List<ProductSoldStat> productsSold) {
+  List<TableRow> createProductSoldStat(List<ProductSoldStat> productsSold, var ttf) {
     List<TableRow> tableRow = [];
 
     tableRow.add(TableRow(children: [
-      Text("Name", style: getTitleStyle()),
-      Text("Paint type", style: getTitleStyle()),
-      Text("Count", style: getTitleStyle()),
-      Text("Quantity", style: getTitleStyle()),
-      Text("Total", style: getTitleStyle()),
-      Text("Unit Price", style: getTitleStyle()),
+      Text("Name", style: getTitleStyle(ttf)),
+      Text("Paint type", style: getTitleStyle(ttf)),
+      Text("Count", style: getTitleStyle(ttf)),
+      Text("Quantity", style: getTitleStyle(ttf)),
+      Text("Total", style: getTitleStyle(ttf)),
+      Text("Unit Price", style: getTitleStyle(ttf)),
     ]));
 
-    tableRow.addAll(productsSold.map((ProductSoldStat productSoldStat) {
-      // print(productSoldStat.product.name);
-      return TableRow(children: [
-        Text(ascii.decode(productSoldStat.product.name.codeUnits)),
-        Text(productSoldStat.product.type ?? "-"),
-        Text(productSoldStat.count.toString() ?? "-"),
-        Text(productSoldStat.quantity.toString() ?? "-"),
-        Text(productSoldStat.totalAmount.toString() ?? "-"),
-        Text(productSoldStat.product.unitPrice.toString() ?? "-"),
 
+    tableRow.addAll(productsSold.map((ProductSoldStat productSoldStat) {
+      return TableRow(children: [
+        Text(productSoldStat.product.name, style: getTitleStyle(ttf)),
+        Text(productSoldStat.product.type ?? "-", style: getTitleStyle(ttf)),
+        Text(productSoldStat.count.toString() ?? "-", style: getTitleStyle(ttf)),
+        Text(productSoldStat.quantity.toString() ?? "-", style: getTitleStyle(ttf)),
+        Text(productSoldStat.totalAmount.toString() ?? "-", style: getTitleStyle(ttf)),
+        Text(productSoldStat.product.unitPrice.toString() ?? "-", style: getTitleStyle(ttf)),
       ]);
     }).toList());
 
